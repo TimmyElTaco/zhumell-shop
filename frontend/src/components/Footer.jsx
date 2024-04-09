c968f39d2f9eebfcbbb0c32088676eee012ce32b
Alberto Guzman <112503817+TimmyElTaco@users.noreply.github.com>
2024-04-09 14:45:29 -0600

    Add links to contact me in the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,24 +5,27 @@ export default function Footer() {
                 <h3 className="text-lg font-bold">Products</h3>
                 <ul>
                     <li>
-                        <a href="#" className="text-sm">Telescopes</a>
+                        <a href="/catalog/telescopes" className="text-sm">Telescopes</a>
                     </li>
                     <li>
-                        <a href="#" className="text-sm">Binoculars</a>
+                        <a href="/catalog/binoculars" className="text-sm">Binoculars</a>
+                    </li>
+                    <li>
+                        <a href="/catalog/accessories" className="text-sm">Accessories</a>
                     </li>
                 </ul>
             </div>
             <div>
-                <h3 className="text-lg font-bold">Support</h3>
+                <h3 className="text-lg font-bold">Made by</h3>
                 <ul>
                     <li>
-                        <a href="#" className="text-sm">Product Manuals</a>
+                        <a href="https://www.linkedin.com/in/luis-guzman-bautista/" target="_blank" rel="nonoreferrer noopener" className="text-sm">LinkedIn</a>
                     </li>
                     <li>
-                        <a href="#" className="text-sm">Warranty Information</a>
+                        <a href="https://github.com/TimmyElTaco" className="text-sm" target="_blank" rel="nonoreferrer noopener">GitHub</a>
                     </li>
                     <li>
-                        <a href="#" className="text-sm">Contact Us</a>
+                        <a href="https://twitter.com/TimmyElTaco" className="text-sm" target="_blank" rel="nonoreferrer noopener">Twitter/X</a>
                     </li>
                 </ul>
             </div>
